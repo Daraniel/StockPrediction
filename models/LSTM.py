ef634bf923f0a813a8f2ec3c66d32297f4ace374
Mojtaba Babayi <babayi.mojtaba.mb@gmail.com>
2020-06-15 16:02:46 +0430

    N next day prediction fixed

--- models/LSTM.py
+++ models/LSTM.py
@@ -33,32 +33,32 @@ class SelfFeedLSTM:
     def __init__(self, data_frame: pd.DataFrame, **kwargs):
         self.model = RawLSTM(lstm_units=100)
         self.data_frame = data_frame
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
         self.__preprocess()
 
     def __preprocess(self, **kwargs):
         self.data = self.data_frame.filter(['close'])
         dataset = np.asarray(self.data, dtype=np.float32).reshape(-1, 1)
 
-        self.training_data_len = int(len(dataset) * 0.8)
-        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.train_len = int(len(dataset) * 0.8)
         scaled_dataset = self.scaler.fit_transform(dataset)
 
-        train_data = scaled_dataset[:self.training_data_len, :]
+        train_data = scaled_dataset[:self.train_len, :]
         self.x_train = []
         self.y_train = []
-        for i in range(60, len(train_data)-1):
+        for i in range(60, len(train_data)):
             self.x_train.append(train_data[i - 60:i, 0])
-            self.y_train.append(train_data[i+1, 0])
+            self.y_train.append(train_data[i, 0])
+
         self.x_train, self.y_train = np.array(self.x_train), np.array(self.y_train)
         self.x_train = np.reshape(self.x_train, (self.x_train.shape[0], self.x_train.shape[1], 1))
-        self.test_data = scaled_dataset[self.training_data_len - 60:, :]
 
+        self.test_data = scaled_dataset[self.train_len - 60:, :]
         self.x_test = []
-        self.y_test = dataset[self.training_data_len:, :]
+        self.y_test = dataset[self.train_len:, :]
         for i in range(60, len(self.test_data)):
             self.x_test.append(self.test_data[i - 60:i, 0])
         self.x_test = np.array(self.x_test)
-
         self.x_test = np.reshape(self.x_test, (self.x_test.shape[0], self.x_test.shape[1], 1))
 
     @tf.function
@@ -76,15 +76,15 @@ class SelfFeedLSTM:
                                                   beta_2=0.999)
 
         self.model.compile(optimizer=self.optimizer, loss=self.loss_fn)
-        self.model.fit(self.x_train, self.y_train, epochs=50)
+        self.model.fit(self.x_train, self.y_train, epochs=10)
 
     def predict(self):
-        # predictions = self.model(self.x_test)  # predict day by day
-        predictions = predict_ndays(self.model, self.x_test, 660).reshape(-1, 1)  # predict n next days
+        predictions = self.model(self.x_test)  # predict day by day
+        # predictions = predict_ndays(self.model, self.x_train, 660).reshape(-1, 1)  # predict n next days
 
         predictions = self.scaler.inverse_transform(predictions)
-        train = self.data[:self.training_data_len]
-        valid = self.data[self.training_data_len:]
+        train = self.data[:self.train_len]
+        valid = self.data[self.train_len:]
         valid['predictions'] = predictions
 
         return train, valid
